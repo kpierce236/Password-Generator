83a78c2d476a8b4443911d596f9bf0b4fbfa14cb
Kevin Pierce <kpierce236@gmail.com>
2023-10-19 18:34:06 -0400

    added prompt for password length

--- Assets/script.js
+++ Assets/script.js
@@ -15,8 +15,18 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   //prompt for password length and check if length is within requirements
+  var passwordLength = prompt("How many characters would you like your password to contain?");
 
-  
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength);
+  } else if (passwordLength < 8) {
+    alert("Password length must be at least 8 characters");
+  } else if (passwordLength > 128) {
+    alert("Password length must be less than 128 characters");
+  } else {
+    alert("Invalid input");
+  }
+
   //confirmation on character types lowercase, uppercase, numeric, and special characters
   // if none slect display message that at least one must be selected
 
