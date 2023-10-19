012f5ae0052c27ec9cb2b3c6926dcce537a91b62
Kevin Pierce <kpierce236@gmail.com>
2023-10-19 18:51:51 -0400

    added password object

--- Assets/script.js
+++ Assets/script.js
@@ -14,21 +14,40 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  //prompt for password length and check if length is within requirements
-  var passwordLength = prompt("How many characters would you like your password to contain?");
 
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    console.log(passwordLength);
-  } else if (passwordLength < 8) {
+  //password length prompt
+  
+  var prompt = prompt("How many characters would you like your password to contain?");
+
+  //password object
+
+    var password = {
+      length: prompt,
+      specialChar: undefined,
+      lowercase: undefined,
+      uppercase: undefined,
+      numeric: undefined
+    };
+
+  //checks if length is within requirements
+
+  if (password.length >= 8 && password.length <= 128) {
+    console.log(length);
+  } else if (password.length < 8) {
     alert("Password length must be at least 8 characters");
-  } else if (passwordLength > 128) {
+  } else if (password.length > 128) {
     alert("Password length must be less than 128 characters");
   } else {
     alert("Invalid input");
   }
 
   //confirmation on character types lowercase, uppercase, numeric, and special characters
-  // if none slect display message that at least one must be selected
+  // if none select display message that at least one must be selected
+  if (window.confirm("Click OK to confirm including special characters.")) {
+    
+  } else {
+
+  }
 
 
   //generates and returns password
